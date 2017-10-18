a7297de11765b64030226cc1707179a16a425827
mariko <lmmlxlk@gmail.com>
2017-10-18 12:26:58 +0900

    modified inquiry, country and add function exchange

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -77,3 +77,61 @@ $(window).on("load",function() {
         });
     });
 });
+
+// RATE
+
+var selecterCurrency = "＄ドル";
+function changeCurrency(){
+  var currency = document.getElementById("currency");
+  selectedCurrency = currency.options[currency.selectedIndex].value;
+  var rateElement = document.getElementById("rate");
+  switch(selectedCurrency) {
+    case "$ドル":
+      // rateElement.value = 77.8;
+      // ('.currency-name').val();
+      usd = document.getElementById('currency-usd').innerHTML;
+      rateElement.value = usd;
+      break;
+    case "€ユーロ":
+      eur = document.getElementById('currency-eur').innerHTML;
+      rateElement.value = eur;
+      break;
+    case "人民元":
+      gbp = document.getElementById('currency-gbp').innerHTML;
+      rateElement.value = gbp;
+      break;
+    default:
+      usd = document.getElementById('currency-usd').innerHTML;
+      rateElement.value = usd;
+   }
+  document.getElementById("buttonExchange").value =
+    selectedCurrency + "から円に両替";
+  document.getElementById("buttonExchange2").value =
+    selectedCurrency +"円から" + selectedCurrency + "に両替"
+}
+function exchange(exchangeType) {
+  var rate = document.getElementById("rate").value;
+  var inMoney = document.getElementById("inMoney").value;
+  var exchangeMoney;
+  var currency;
+  if (exchangeType == "toYen") {
+    exchangeMoney = inMoney * rate;
+    currency = "円"
+  }
+  else {
+    exchangeMoney = inMoney / rate;
+    currency = selectedCurrency;
+  }
+  exchangeMoney =Math.round(exchangeMoney * 100) / 100;
+  if (isNaN(exchangeMoney)) {
+    alert("数値を入力してください");
+    exchangeMoney = "----";
+  }
+  if (rate <= 0) {
+    alert("為替レートは0より大きな数値を入力してください");
+    exchangeMoney = "-----";
+  }
+
+  var outMoneyElement =document.getElementById("outMoney");
+  outMoneyElement.innerHTML = exchangeMoney + currency;
+}
